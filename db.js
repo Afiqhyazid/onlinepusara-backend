62be9444c9e92d9d80b395aa039233af9873c5dd
Afiqhyazid <Afiqahyazid022@gmail.com>
2025-11-17 13:04:04 +0800

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,11 @@ const sql = require('mssql');
 const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  server: process.env.DB_HOST, // You may need to replace 'localhost' with your actual SQL Server IP address
+  server: process.env.DB_HOST, // Replace with your SQL Server IP or hostname
   database: process.env.DB_DATABASE,
   options: {
-    encrypt: false, // For Azure SQL Database, true
-    trustServerCertificate: true // Change to true for local dev / self-signed certs
+    encrypt: false, // For Azure SQL Database, set true
+    trustServerCertificate: true // Set true for local dev / self-signed certs
   },
   port: parseInt(process.env.DB_PORT, 10)
 };
@@ -23,22 +23,3 @@ const poolPromise = new sql.ConnectionPool(config)
   });
 
 module.exports = { sql, poolPromise };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
